7c5f9c31e79e83c6ffa4c8b59621c0600b2e3fc7
fredkamm <fred.kamm95@gmail.com>
2023-01-04 10:54:43 -0500

    positioning and font size is complete for the services section, need to update the data for it later

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -8,20 +8,20 @@ const Services = () => {
     <section id="services" className="section bg-gradient-to-t from-primary to-primary2">
       <div className="container mx-auto">
         <div className="flex flex-col items-center text-center">
-          <h2 className="section-title before:content-services relative before:absolute before:opacity-40 before:-top-[2rem] before:-left-28 before:hidden before:lg:block">
-            What I do for clients
+          <h2 className="text-[36px] font-bold py-7 text-gray-800 xl:text-[50px] lg:text-[42px] md:text-[38px]">
+            Services
           </h2>
           <p className="subtitle">These are the services that I provide</p>
         </div>
-        <div className="grid pb-8 lg:grid-cols-3 gap-8">
+        <div className="grid px-3 pb-8 lg:grid-cols-3 gap-8">
           {services.map((service, index) => {
             const { icon, name, description } = service;
             return (
               <div className="border-2 border-text p-6 rounded-2xl" key={index}>
-                <div className="text-text rounded-sm w-12 h-12 flex justify-center items-center mb-14 text-[42px]">
+                <div className="text-text rounded-sm w-12 h-12 flex justify-center items-center mb-7 text-[42px]">
                   {icon}
                 </div>
-                <h4 className="text-xl font-medium mb-2">{name}</h4>
+                <h2 className="text-3xl font-medium mb-2">{name}</h2>
                 <p>{description}</p>
               </div>
             );
